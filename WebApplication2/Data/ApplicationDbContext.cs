68dfce7ca271dd5275e8e17f83882ec4324d3401
ABC <ABC@DESKTOP-0G1MB4B>
2023-04-28 09:57:28 +0300

    Add project files.

---
+++ WebApplication2/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WebApplication2.Models.Machine> Machine { get; set; } = default!;
+        public DbSet<WebApplication2.Models.Materials> Materials { get; set; } = default!;
+    }
+}
